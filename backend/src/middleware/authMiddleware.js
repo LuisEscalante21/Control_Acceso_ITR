4a611480ec1672f32b4f24076c731c7e39bdc6b7
Luis <20230352@ricaldone.edu.sv>
2025-09-10 00:56:29 +0200

    Rutas protegidas correcion de error

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"; 
 import { config } from "../config.js";
 import Employee from "../models/Employees.js";
 import Coordinator from "../models/Coordinators.js";
@@ -6,12 +6,21 @@ import Administrator from "../models/Administrators.js";
 
 export const authMiddleware = async (req, res, next) => {
   try {
-    // Tomar token desde cookie o header Authorization
     const token = req.cookies.authToken || req.headers.authorization?.split(' ')[1];
-
     if (!token) return res.status(401).json({ message: "No autenticado" });
 
     const decoded = jwt.verify(token, config.JWT.secret);
+
+    // Caso especial para Admin del .env
+    if (decoded.isReadOnly && decoded.id === "Admin") {
+      req.user = {
+        _id: "Admin",
+        userType: "administrator",
+        name: "Admin ReadOnly",
+        isReadOnly: true,
+      };
+      return next();
+    }
 
     let user = null;
 
